ee8ab11aebd5b02e229da655cf3c51d1671b36ce
pepster98 <spencer.pepe@yahoo.com>
2016-10-08 21:53:59 -0400

    started destroyer class code and improved gitignore

--- Destroyer.cs
+++ Destroyer.cs
@@ -11,6 +11,43 @@ namespace Parallel
 {
     public class Destroyer
     {
-        
+        private Vector2 _pos = new Vector2(0, 0);
+        private Vector2 _velocity = new Vector2(0, 0);
+        public Vector2 position{
+            get {
+                return _pos;
+            }
+        }
+        public void move(Direction d) {
+            //Basic velocity movemetn setup.
+            switch (d)
+            {
+                case Direction.Up:
+                    _velocity = new Vector2(0, -0.1f);
+                    break;
+
+                case Direction.Right:
+                    _velocity = new Vector2(0.1f, 0);
+                    break;
+
+                case Direction.Down:
+                    _velocity = new Vector2(0, 0.1f);
+                    break;
+
+                case Direction.Left:
+                    _velocity = new Vector2(-0.1f, 0);
+                    break;
+
+            }
+        }
+        public void tryStop(){
+
+        }
+
+        public enum MovementType{
+            //TODO: HOW DO WE WANT THE SHIP TO MOVE? PURE SLIPPERY ASTROIDS STYLE MOVEMENT MIGHT BE 
+            //TOO HARD FOR THIS TYPE OF GAME
+            accelerate, decelerate
+        }
     }
 }
